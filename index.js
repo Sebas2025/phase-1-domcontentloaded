6fa97573534f1b132c672a588db9416885bb8d6a
SebastainBernal <sebasair2025@gmail.com>
2022-12-20 16:31:42 -0700

    commit tree 706b5d4095a0649931d88904d00c87b92bd1d928

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-// Your code goes here
+
+
+document.addEventListener("DOMContentLoaded", function(){
+    rewrite()
+    });
+
+
+function rewrite(){
+   let p =  document.getElementById('text').textContent =
+    "This is really cool!";
+   
+}
